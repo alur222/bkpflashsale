bea73810e5ffc8f3c3394811ada0a6378e98a85a
Raul Torrefiel JR <aluroticooo@gmail.com>
2025-09-25 19:32:02 +0800

    create new table for purchases and adjust initial seed

--- backend/setup.sql
+++ backend/setup.sql
@@ -1,4 +1,3 @@
--- Create inventory table
 CREATE TABLE inventory (
   item_id INT PRIMARY KEY,
   stock_left INT NOT NULL,
@@ -6,12 +5,17 @@ CREATE TABLE inventory (
   ends_at TIMESTAMPTZ NOT NULL
 );
 
--- Seed with configurable data
--- Modify the dates and stock as needed
+CREATE TABLE purchases (
+  id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+  user_id TEXT UNIQUE NOT NULL,
+  item_id INT NOT NULL,
+  created_at TIMESTAMPTZ DEFAULT NOW()
+);
+
 INSERT INTO inventory (item_id, stock_left, starts_at, ends_at) VALUES (
   1, 
   100, 
-  NOW() + INTERVAL '1 hour',  -- Sale starts in 1 hour
+  NOW() - INTERVAL '1 hour',  -- I have to adjust this so we can start purchasing immediately
   NOW() + INTERVAL '25 hours' -- Sale ends in 25 hours (24 hour duration)
 );
 
